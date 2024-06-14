78bc4467a7a7730b025c94abe05a40534841a057
sabino <982190+sabino@users.noreply.github.com>
2024-06-14 17:46:24 -0300

    Add setup.py for sabcli package with click as a dependency and console script entry point.

---
+++ setup.py
@@ -0,0 +1,16 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='sabcli',
+    version='0.1.0',
+    packages=find_packages(where='src'),
+    package_dir={'': 'src'},
+    install_requires=[
+        'click',
+    ],
+    entry_points={
+        'console_scripts': [
+            'sabcli=sabcli.cli:cli',
+        ],
+    },
+)
